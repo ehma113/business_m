d72789ed5f74dbd55269b006b2824b982d537968
ehma113 <ehma1023@gmail.com>
2026-04-23 14:00:36 +0100

    Try https

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // Removed the VITE_API_URL line for now to force it to use PythonAnywhere
-  baseURL: 'http://ehma50.pythonanywhere.com/api/'
+  baseURL: 'https://ehma50.pythonanywhere.com/api/'
 });
 
-// ✅ ADD THIS LINE to tell the browser to allow the connection
-apiClient.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-// This interceptor will automatically add the auth token to every request
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('authToken');
